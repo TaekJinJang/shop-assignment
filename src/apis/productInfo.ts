df278f4f484f99187aa0c066f8626f3005f08eda
Taekjin Jang <xorwls1500@naver.com>
2023-10-07 15:50:04 +0900

    feat: json-server 배포

--- src/apis/productInfo.ts
+++ src/apis/productInfo.ts
@@ -1,8 +1,9 @@
 import {httpClient} from './instance';
+import * as Type from 'types/products';
 
-const DB_JSON_PATH = 'mock/data.json';
+const DB_JSON_PATH = 'db.json';
 
 export const getProductInfo = async () => {
-    const response = await httpClient.get(DB_JSON_PATH);
+    const response = await httpClient.get<Type.ResponseData>(DB_JSON_PATH);
     return response.data;
 };
