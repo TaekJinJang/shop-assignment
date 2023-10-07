246ba790933c27b3dea00c571e8b497682226357
Taekjin Jang <xorwls1500@naver.com>
2023-10-07 15:49:09 +0900

    feat: responseData 타입 추가

---
+++ src/types/products.ts
@@ -0,0 +1,13 @@
+export interface ResponseData {
+    id: string;
+    category_code: string;
+    status: string;
+    owner_product_code: string;
+    product_name: string;
+    price: string;
+    main_image: string;
+    origin: string;
+    brand: string;
+    model: string;
+    keywords: Array<string>;
+}
