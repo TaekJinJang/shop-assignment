34761d0ee4ada5e786c8e20181784eed93fb38f0
Taekjin Jang <xorwls1500@naver.com>
2023-10-08 22:19:13 +0900

    feat: 콤보박스 options 상수화

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,8 @@
 export const BASE_URL = 'http://localhost:4000/';
-export const TOTAL_ITEMS = 500;
+export const ITEMS_COUNT_PER_PAGE = [
+    {value: 5, name: '5개'},
+    {value: 10, name: '10개'},
+    {value: 30, name: '30개'},
+    {value: 50, name: '50개'},
+    {value: 100, name: '100개'},
+];
